ae3a8778364468c462fd22cfe3fd7a8880776541
Tjaden Hess <tah83@cornell.edu>
2018-11-04 18:19:26 -0500

    Add test and fix import parsing

--- zokrates_core/src/parser/mod.rs
+++ zokrates_core/src/parser/mod.rs
@@ -10,8 +10,17 @@ lalrpop_mod!(parser, "/src/parser/parser.rs");
 
 #[test]
 fn test_parsergen() {
-    let expr = parser::ExprParser::new()
-        .parse::<FieldPrime>("22 * 44 + 66")
+    let prog =
+r#"def foo(field a) -> (field, field, field, field):
+	field b = 12*a
+	return a, 2*a, 5*b, a*b
+
+def main(field i) -> (field):
+	field x, field y, field z, field t = foo(i)
+  return 1"#;
+
+    let expr = parser::ProgramParser::new()
+        .parse::<FieldPrime>(prog)
         .unwrap();
     //assert_eq!(&format!("{:?}", expr), "((22 * 44) + 66)");
 }
